24dc8f1498c2a66905fcc0c2ac85b011acfc7efa
FiveSaven <racconrolly@gmail.com>
2025-03-08 17:58:59 +0300

    newtonjson

--- MTSolution/Program.cs
+++ MTSolution/Program.cs
@@ -1,190 +1,127 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text.Json;
+using Newtonsoft.Json;
+using System.Xml;
 
 class Program
 {
-    static Dictionary<string, Host> hosts = new();
-    static Dictionary<string, VirtualMachine> virtualMachines = new();
-
     static void Main()
     {
-        try
-        {
-            // Читаем JSON из файла
-            string jsonInput = File.ReadAllText("input.json");
-            var inputRounds = JsonSerializer.Deserialize<List<InputData>>(jsonInput);
+        string input = Console.In.ReadToEnd().Trim();
+        var request = JsonConvert.DeserializeObject<InputData>(input);
+        var response = new OutputData();
+        var vmPlacement = new Dictionary<string, string>();
+        var migrations = new List<Migration>();
 
-            if (inputRounds == null)
+        var hostUsage = request.Hosts.ToDictionary(h => h.Key, h => new Resource(h.Value.Cpu, h.Value.Ram));
+
+        foreach (var vm in request.VirtualMachines)
+        {
+            bool placed = false;
+            foreach (var host in hostUsage.OrderBy(h => h.Value.CpuUsage))
             {
-                Console.WriteLine("Ошибка: Невозможно загрузить входные данные.");
-                return;
-            }
-
-            foreach (var roundData in inputRounds)
-            {
-                // Инициализируем хосты только на первом раунде
-                if (hosts.Count == 0)
-                    hosts = roundData.hosts;
-
-                // Обновляем список ВМ согласно diff
-                UpdateVirtualMachines(roundData.virtual_machines, roundData.diff);
-                var allocationResult = AllocateVMs();
-
-                // рез в stdout
-                string outputJson = JsonSerializer.Serialize(allocationResult, new JsonSerializerOptions { WriteIndented = true });
-                Console.WriteLine(outputJson);
-            }
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine($"Ошибка обработки JSON: {ex.Message}");
-        }
-    }
-
-    static void UpdateVirtualMachines(Dictionary<string, VirtualMachine> newVMs, Diff diff)
-    {
-        // Удаляем ВМ, если они отсутствуют в текущем раунде
-        foreach (var vm in virtualMachines.Keys.ToList())
-        {
-            if (!newVMs.ContainsKey(vm))
-                virtualMachines.Remove(vm);
-        }
-
-       
-        if (diff?.add?.virtual_machines != null)
-        {
-            foreach (var vmId in diff.add.virtual_machines)
-            {
-                if (newVMs.ContainsKey(vmId))
-                    virtualMachines[vmId] = newVMs[vmId]; // Добавляем новые ВМ
-            }
-        }
-
-       
-        if (diff?.remove?.virtual_machines != null) 
-        {
-            foreach (var vmId in diff.remove.virtual_machines)
-            {
-                virtualMachines.Remove(vmId); // Удаляем ВМ, если они указаны в diff.remove
-            }
-        }
-    }
-
-    static OutputData AllocateVMs()
-    {
-        Dictionary<string, List<string>> allocations = new();
-        List<string> allocationFailures = new();
-        Dictionary<string, Migration> migrations = new();
-        Dictionary<string, Host> remainingResources = hosts.ToDictionary(h => h.Key, h => new Host(h.Value));
-        foreach (var vm in virtualMachines)
-        {
-            string bestHost = FindBestHost(vm.Value, remainingResources);
-            if (bestHost != null)
-            {
-                if (!allocations.ContainsKey(bestHost))
-                    allocations[bestHost] = new List<string>();
-
-                allocations[bestHost].Add(vm.Key);
-                remainingResources[bestHost].cpu -= vm.Value.cpu;
-                remainingResources[bestHost].ram -= vm.Value.ram;
-            }
-            else
-            {
-                allocationFailures.Add(vm.Key);
-            }
-        }
-        return new OutputData
-        {
-            allocations = allocations,
-            allocation_failures = allocationFailures,
-            migrations = migrations
-        };
-    }
-    static string FindBestHost(VirtualMachine vm, Dictionary<string, Host> resources)
-    {
-        string bestHost = null;
-        double bestUtilization = double.MaxValue;
-
-        foreach (var host in resources)
-        {
-           
-            if (host.Value.cpu < vm.cpu || host.Value.ram < vm.ram) // Проверка, что хост может разместить ВМ по CPU и RAM
-                continue;
-
-            //утилизация хоста
-            double cpuUsage = (host.Value.cpu - vm.cpu) / (double)hosts[host.Key].cpu;
-            double ramUsage = (host.Value.ram - vm.ram) / (double)hosts[host.Key].ram;
-            double avgUtilization = (cpuUsage + ramUsage) / 2;
-
-            // Утилизация хоста должна быть в пределах от 78% до 82%, в идеале 79.сотые до 80.сотые
-            if (avgUtilization >= 0.78 && avgUtilization <= 0.82)
-            {
-                if (avgUtilization < bestUtilization)
+                if (host.Value.CanHost(vm.Value))
                 {
-                    bestUtilization = avgUtilization;
-                    bestHost = host.Key;
+                    host.Value.Allocate(vm.Value);
+                    vmPlacement[vm.Key] = host.Key;
+                    placed = true;
+                    break;
+                }
+            }
+            if (!placed)
+            {
+                response.FailedPlacements.Add(vm.Key);
+            }
+        }
+
+        // Миграция ВМ при перегрузке хоста
+        foreach (var host in hostUsage)
+        {
+            if (host.Value.CpuUsage > 0.8)
+            {
+                var vmsToMigrate = vmPlacement.Where(v => v.Value == host.Key).ToList();
+                foreach (var vm in vmsToMigrate)
+                {
+                    foreach (var targetHost in hostUsage.OrderBy(h => h.Value.CpuUsage))
+                    {
+                        if (targetHost.Key != host.Key && targetHost.Value.CanHost(request.VirtualMachines[vm.Key]))
+                        {
+                            host.Value.Deallocate(request.VirtualMachines[vm.Key]);
+                            targetHost.Value.Allocate(request.VirtualMachines[vm.Key]);
+                            vmPlacement[vm.Key] = targetHost.Key;
+                            migrations.Add(new Migration { Vm = vm.Key, From = host.Key, To = targetHost.Key });
+                            break;
+                        }
+                    }
                 }
             }
         }
 
-        return bestHost;
+        response.Placements = vmPlacement;
+        response.Migrations = migrations;
+        Console.WriteLine(JsonConvert.SerializeObject(response, new JsonSerializerSettings { Formatting = Newtonsoft.Json.Formatting.Indented }));
     }
-}
-class Host
-{
-    public int cpu { get; set; }
-    public int ram { get; set; }
-
-    public Host() { }
-
-    public Host(Host other)
-    {
-        cpu = other.cpu;
-        ram = other.ram;
-    }
-}
-
-class VirtualMachine
-{
-    public int cpu { get; set; }
-    public int ram { get; set; }
 }
 
 class InputData
 {
-    public Dictionary<string, Host> hosts { get; set; } = new();
-    public Dictionary<string, VirtualMachine> virtual_machines { get; set; } = new();
-    public Diff diff { get; set; } = new();
+    [JsonProperty("hosts")] public Dictionary<string, Resource> Hosts { get; set; }
+    [JsonProperty("virtual_machines")] public Dictionary<string, Resource> VirtualMachines { get; set; }
+    [JsonProperty("diff")] public DiffChanges Diff { get; set; }
 }
 
-class Diff
+class DiffChanges
 {
-    public Add add { get; set; } = new();
-    public Remove remove { get; set; } = new();
+    [JsonProperty("add")] public DiffAdd Add { get; set; }
 }
 
-class Add
+class DiffAdd
 {
-    public string[] virtual_machines { get; set; } = Array.Empty<string>();
-}
-
-class Remove
-{
-    public string[] virtual_machines { get; set; } = Array.Empty<string>();
+    [JsonProperty("virtual_machines")] public List<string> VirtualMachines { get; set; }
 }
 
 class OutputData
 {
-    public Dictionary<string, List<string>> allocations { get; set; } = new();
-    public List<string> allocation_failures { get; set; } = new();
-    public Dictionary<string, Migration> migrations { get; set; } = new();
+    [JsonProperty("placements")] public Dictionary<string, string> Placements { get; set; } = new();
+    [JsonProperty("failed_placements")] public List<string> FailedPlacements { get; set; } = new();
+    [JsonProperty("migrations")] public List<Migration> Migrations { get; set; } = new();
 }
 
 class Migration
 {
-    public string from { get; set; }
-    public string to { get; set; }
+    [JsonProperty("vm")] public string Vm { get; set; }
+    [JsonProperty("from")] public string From { get; set; }
+    [JsonProperty("to")] public string To { get; set; }
+}
+
+class Resource
+{
+    [JsonProperty("cpu")] public int Cpu { get; set; }
+    [JsonProperty("ram")] public int Ram { get; set; }
+    public int CpuUsed { get; private set; }
+    public int RamUsed { get; private set; }
+
+    public Resource(int cpu, int ram)
+    {
+        Cpu = cpu;
+        Ram = ram;
+        CpuUsed = 0;
+        RamUsed = 0;
+    }
+
+    public bool CanHost(Resource vm) => CpuUsed + vm.Cpu <= Cpu && RamUsed + vm.Ram <= Ram;
+    public void Allocate(Resource vm)
+    {
+        CpuUsed += vm.Cpu;
+        RamUsed += vm.Ram;
+    }
+    public void Deallocate(Resource vm)
+    {
+        CpuUsed -= vm.Cpu;
+        RamUsed -= vm.Ram;
+    }
+    public double CpuUsage => (double)CpuUsed / Cpu;
 }
